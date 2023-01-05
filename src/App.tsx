96164f23efb6c5604e068e35a1750938910b5f00
RotaruPaulCristian <cristian.rotaru96@yahoo.com>
2023-01-05 21:46:01 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ src/pages/Store.tsx
@@ -0,0 +1,20 @@
+import storeItems from "../data/items.json";
+import { Row, Col } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
+
+const Store = () => {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Store;
